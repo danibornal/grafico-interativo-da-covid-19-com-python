632de87e13526bbabbfd55cde499f81dddf2bc50
Daniela Bornal <116318070+danibornal@users.noreply.github.com>
2023-08-03 14:38:45 -0300

    Update codigobase (1).py

--- codigobase (1).py
+++ codigobase (1).py
@@ -9,32 +9,34 @@ Original file is located at
 
 import pandas as pd
 import plotly.express as px
-import streamlit as st
+import streamlit as st 
 
-#Lendo o dataset
+#streamlit run codigoBase.py
+
+#LENDO O DATASET
 df = pd.read_csv('https://raw.githubusercontent.com/wcota/covid19br/master/cases-brazil-states.csv')
 
-#Mellhorando o nome das colunas da tabela
-df = df.rename(columns={'newDeaths': 'Novos óbitos', 'newCases': 'Novos casos', 'deaths_per_100k_inhabitants': 'Óbitos por 100mil habitantes', 'totalCases_per_100k_inhabitants': 'Casos por 100mil habitantes'})
+#MELHORANDO O NOME DAS COLUNAS DA TABELA
+df = df.rename(columns={'newDeaths': 'Novos óbitos','newCases': 'Novos casos','deaths_per_100k_inhabitants': 'Óbitos por 100 mil habitantes','totalCases_per_100k_inhabitants':'Casos por 100 mil habitantes'})
 
-#Seleção de Estados
+#SELECÃO DO ESTADO
 estados = list(df['state'].unique())
-state = st.sidebar.selectbox('Qual Estado?', estados)
+state = st.sidebar.selectbox('Qual estado?', estados)
 
-#Seleção da Coluna
-#column = 'Casos por 100mil habitantes'
-colunas = ['Novos óbitos', 'Novos casos', 'Óbitos por 100mil habitantes', 'Casos por 100mil habitantes']
+#SELEÇÃO DA COLUNA
+#column ='Casos por 100 mil habitantes'
+colunas = ['Novos óbitos','Novos casos','Óbitos por 100 mil habitantes','Casos por 100 mil habitantes']
 column = st.sidebar.selectbox('Qual tipo de informação?', colunas)
 
-#Seleçõ das linhas que pertencem ao Estado
-df= df[df['state'] == state]
+#SELEÇÃO DAS LINHAS QUE PERTECEM AO ESTADO 
+df = df[df['state'] == state]
 
-fig = px.line(df, x='date', y= column, title = column + '-'+ state)
-fig.update_layout( xaxis_title = 'Data', yaxis_title =column.upper(), title = {'x': 0.5} )
+fig = px.line(df, x="date", y=column, title=column + ' - ' + state)
+fig.update_layout( xaxis_title='Data', yaxis_title=column.upper(), title = {'x':0.5})
 
 st.title('DADOS COVID - BRASIL')
-st.write('Nessa aplicação, o usuário tem a opção de escolher o estado e o tipo de informação para mostrar o gráfico. Utilize o menu lateral para alterar a mostragem')
+st.write('Nessa aplicação, o usuário tem a opção de escolher o estado e o tipo de informação para mostrar o gráfico. Utilize o menu lateral para alterar a mostragem.')
 
 st.plotly_chart(fig, use_container_width=True)
 
-st.caption('Os dados foram obtidos a partir do site : https://github.com/wcota/covid19br.git')
+st.caption('Os dados foram obtidos a partir do site: https://github.com/wcota/covid19br')
